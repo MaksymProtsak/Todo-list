499b144b333470c61f11f809e5787e380b564225
Maksym Protsak <maksym.protsak@gmail.com>
2024-11-02 13:36:35 +0200

    Created urls: task-list, task-create, task-update, task-delete, tag-list,tag-create, tag-update, tag-delete.

---
+++ todo_app/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path
+
+from .views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    ToggleTaskStatusView,
+)
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-crete"),
+
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update",
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete",
+    ),
+    path(
+        "tasks/<int:pk>/toggle-assign/",
+        ToggleTaskStatusView.as_view(),
+        name="toggle-task-status",
+    ),
+]
+
+app_name = "todo_app"
